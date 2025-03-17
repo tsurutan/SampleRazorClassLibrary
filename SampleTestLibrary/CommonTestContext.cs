4e754ca3dfc27b202fc544012f87ff08523bad0d
tsurutan <tsurutan.android@gmail.com>
2025-03-17 11:26:03 +0900

    fix: 🐛 publis_ui.yml

--- SampleTestLibrary/CommonTestContext.cs
+++ SampleTestLibrary/CommonTestContext.cs
@@ -1,14 +1,13 @@
-// using Bunit;
-// using Xunit;
+using Bunit;
+using Xunit;
 
 namespace SampleTestLibrary;
 
-// public class CommonTestContext: TestContext
-public class CommonTestContext
+public class CommonTestContext: TestContext
 {
     public string Message { get; set; } = "Hello, World!";
     public void AssertHelloWorld()
     {
-        // Assert.Equal("Hello, World!", Message);
+        Assert.Equal("Hello, World!", Message);
     }
 }
